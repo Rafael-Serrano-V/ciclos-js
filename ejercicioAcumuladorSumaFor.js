9e7bfe0d4cfcdc128d9a92cf2d2e603dc3d535d0
Rafael-Serrano-V <rafaelmserranov@gmail.com>
2024-07-13 18:29:31 -0400

    Ejercicio Suma Acumulativa - for

---
+++ ejercicioAcumuladorSumaFor.js
@@ -0,0 +1,8 @@
+// Sumar los primeros 5 numeros (for)
+let maximo = 5, acumuladorSuma = 0;
+for(let numero = 1; numero <= maximo; numero++) {
+    // Realizamos la suma parcial
+    acumuladorSuma += numero;
+    console.log(acumuladorSuma);
+
+}console.log(acumuladorSuma);
